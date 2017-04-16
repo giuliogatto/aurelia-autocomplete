aa9fd4d7c0e41ae93ea4cac208bb4451ac3721fa
Giulio Gatto <giuliogatto@gmail.com>
2017-04-16 09:18:57 +0200

    First commit

---
+++ autocomplete.js
@@ -0,0 +1,27 @@
+import {bindable} from 'aurelia-framework';
+
+@bindable('suggestions')
+@bindable('property')
+export class AutocompleteCustomElement{
+
+	showSuggestion = false;
+
+	selectSuggestion(suggestion){
+		this.property = suggestion;
+        this.showSuggestion = 0;
+	}
+
+	keyPressed(evt) {
+    	this.property.length > 1 ? this.showSuggestion = 1 : this.showSuggestion = 0;
+    	
+        this.suggestionstoshow = this.suggestions.filter(
+            suggestion => {
+                if(suggestion.toLowerCase().indexOf(this.property.toLowerCase()) > -1)return true;
+        	}
+        );
+
+    	return true;
+    }
+
+   
+}
